c8e83485c5c1cbad98984e8d1d8dd80b7f25d0e6
Computer Virus <edwardrainfall@gmail.com>
2024-07-03 16:37:50 -0600

    Added ToSuperscript and ToSubscript methods to Convert class.

--- MyLib.cs
+++ MyLib.cs
@@ -13,6 +13,54 @@ namespace MyLibrary {
 		public class Convert() {
 
 			/// <summary>
+			///		<para>Converts a non-negative single digit <see cref="int"/> <paramref name="number"/> into a <see cref="char"/> containing the <paramref name="number"/> in superscript.</para>
+			///		<para>Reutrns <see langword="default"/> if <paramref name="number"/> is not a non-negative single digit <see cref="int"/>.</para>
+			///		<para>Note that not all superscript <see cref="char"/>s display properly in <see cref="System.Console"/> by default.</para>
+			///		<para><see cref="ToSuperscript"/> must have exactly 1 <see langword="param"/>.</para>
+			/// </summary>
+			/// <param name="number"></param>
+			/// <returns></returns>
+			public static char ToSuperscript(int number) {
+				switch (number) {
+					default:
+						return default;
+					case 0:
+						return (char)0x2070;
+					case 1:
+						return (char)0xB9;
+					case 2:
+						return (char)0xB2;
+					case 3:
+						return (char)0xB3;
+					case 4:
+						return (char)0x2074;
+					case 5:
+						return (char)0x2075;
+					case 6:
+						return (char)0x2076;
+					case 7:
+						return (char)0x2077;
+					case 8:
+						return (char)0x2078;
+					case 9:
+						return (char)0x2079;
+
+				}
+			}
+
+			/// <summary>
+			///		<para>Converts a single digit <see cref="int"/> <paramref name="number"/> into a <see cref="char"/> containing the <paramref name="number"/> in subscript.</para>
+			///		<para>Reutrns <see langword="default"/> if <paramref name="number"/> is not a non-negative single digit <see cref="int"/>.</para>
+			///		<para>Note that no subscript <see cref="char"/> displays properly in <see cref="System.Console"/> by default.</para>
+			///		<para><see cref="ToSubscript"/> must have exactly 1 <see langword="param"/>.</para>
+			/// </summary>
+			/// <param name="number"></param>
+			/// <returns></returns>
+			public static char ToSubscript(int number) {
+				return (char)(0x2080 + number);
+			}
+
+			/// <summary>
 			///		<para>Converts an <see cref="int"/> <paramref name="number"/> into a <see cref="string"/> containing the <paramref name="number"/>'s english ordinal.</para>
 			///		<para>Returns <see langword="null"/> if the <paramref name="number"/> has no english ordinal.</para>
 			///		<para><see cref="Ordinal"/> must have exactly 1 <see langword="param"/>.</para>
